d174ad1b59944151c9e70cfaf6f0873c0a5bdab1
URBOP <josequintero@joses-macbook-pro.local>
2016-01-03 13:09:11 -0600

    Initial Commit

---
+++ Ody/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ody
+//
+//  Created by Jose Quintero on 1/3/16.
+//  Copyright © 2016 Quintero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
